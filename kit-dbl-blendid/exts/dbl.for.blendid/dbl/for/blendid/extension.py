a6a3a0145c3fa8736d1be1ff1f6574acc5f09c67
yizhouzhao <yizhou_zhao@berkeley.edu>
2023-05-08 10:38:21 -0700

    add controller

--- kit-dbl-blendid/exts/dbl.for.blendid/dbl/for/blendid/extension.py
+++ kit-dbl-blendid/exts/dbl.for.blendid/dbl/for/blendid/extension.py
@@ -1,6 +1,16 @@
 import omni.ext
 import omni.ui as ui
+import omni.timeline
+import omni.kit.app
 import carb
+
+# UI
+from .ui.style import julia_modeler_style
+from .ui.custom_multifield_widget import CustomMultifieldWidget
+from .ui.custom_bool_widget import CustomBoolWidget
+
+# Robot
+from .ur3e.ur3e import UR3E
 
 # Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
 # instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
@@ -19,10 +29,122 @@ class DblForBlendidExtension(omni.ext.IExt):
         self._window = ui.Window("For blendid", width=300, height=300)
         with self._window.frame:
             with ui.VStack():
-                ui.Button("Add", height = 20, clicked_fn=self.debug)
+                ui.Button("Debug", height = 20, clicked_fn=self.debug)
+
+                ui.Line(height = 2)
+                ui.Button("Register Physics Event", height = 50, clicked_fn=self.register_physics_event)
+                with ui.HStack(height = 20): 
+                    ui.Label("Robot Prim Path:", width = 200)
+                    self.robot_path_widget = ui.StringField(width = 300)
+                    self.robot_path_widget.model.set_value("/World/ur3e")
+                
+                ui.Spacer(height = 9)
+                ui.Label("End Effector", height = 20)
+                with ui.HStack(height = 20):
+                    self.ee_pos_widget = CustomMultifieldWidget(
+                        label="Transform",
+                        default_vals=[0, 0, 0],
+                        height = 20,
+                    )
+                ui.Spacer(height = 9)
+                with ui.HStack(height = 20):
+                    self.ee_ori_widget = CustomMultifieldWidget(
+                        label="Orient (Euler)",
+                        default_vals=[90, 0.0, 90],
+                        height = 20,
+                    )
+                ui.Spacer(height = 9)
+                ui.Button("Update EE Target", height = 20, clicked_fn=self.update_ee_target)
+                ui.Button("Open/Close Gripper", height = 20, clicked_fn=self.toggle_gripper)
+
+
+        # robot
+        self.robot = None
+        self.controller = None  
+        self.event_t = 0.0
+
+        # stream
+        self._is_stopped = True
+        self._tensor_started = False   
 
     def on_shutdown(self):
         print("[dbl.for.blendid] dbl for blendid shutdown")
 
+    ########################## events #######################################################
+    def register_physics_event(self):
+        print("register_physics_event")
+        
+        # timeline
+        stream = omni.timeline.get_timeline_interface().get_timeline_event_stream()
+        self._timeline_sub = stream.create_subscription_to_pop(self._on_timeline_event)
+
+    def _on_timeline_event(self, event):
+        if event.type == int(omni.timeline.TimelineEventType.PLAY):
+            self._physics_update_sub = omni.physx.get_physx_interface().subscribe_physics_step_events(self._on_physics_step)
+            self._is_stopped = False
+
+        elif event.type == int(omni.timeline.TimelineEventType.STOP):
+            self._physics_update_sub = None
+            self._timeline_sub = None
+
+            self._is_stopped = True
+            self._tensor_started = False
+
+            self.robot = None
+            self.controller = None  
+        
+
+    def _can_callback_physics_step(self) -> bool:
+        if self._is_stopped:
+            return False
+
+        if self._tensor_started:
+            return True
+
+        self._tensor_started = True
+        self.set_robot()
+        return True 
+    
+    def _on_physics_step(self, dt):
+        self.event_t += dt # update time
+
+        if not self._can_callback_physics_step():
+            return
+
+        if self.controller:
+            # print("_on_physics_step")
+            self.controller.forward()
+
+
+            if self.event_t >= 1.0:
+                # update joint info 
+                # self.update_robot_ui()
+                self.event_t = 0.0
+    
+    ########################### robot #######################################################
+    def set_robot(self):
+        print("set_robot")
+
+        # set robot
+        prim_path = self.robot_path_widget.model.as_string
+        self.robot = UR3E(prim_path = prim_path)
+        self.robot.initialize()
+        print("robot_info", self.robot.num_dof)
+        print("robot_dof_names", len(self.robot.dof_names), self.robot.dof_names)
+        print("robot_gripper", self.robot.gripper._gripper_joint_num)
+
+        # set controller
+        # self.controller = CoffeeMakerController("task_controller", self.robot, connect_server=self.server_widget.value)
+
+    ############################################# Robot #######################################
+    def update_ee_target(self):
+        print("update_ee_target")
+
+    def toggle_gripper(self):
+        print("Toggle Gripper")
+        if self.controller:
+            event = "open" if self.controller.event == "close" else "close"
+            self.controller.update_event(event) 
+ 
     def debug(self):
         print(f"[dbl.for.blendid] debug")
---
+++ kit-dbl-blendid/exts/dbl.for.blendid/dbl/for/blendid/ur3e/ur3e_controller.py
@@ -0,0 +1,213 @@
+import omni.usd
+
+from omni.isaac.core.controllers import BaseController
+from omni.isaac.core.utils.stage import get_stage_units
+from omni.isaac.core.prims import XFormPrim
+
+from .ur3e import UR3E
+from .rmpflow_controller import RMPFlowController
+import numpy as np
+from .numpy_utils import *
+from .utils import regulate_degree, get_transform_mat_from_pos_rot, generate_slerp_action_sequence
+
+
+class Ue3R140Controller(BaseController):
+    def __init__(self, name: str, robot: UR3E, connect_server = False) -> None: 
+        BaseController.__init__(self, name=name)
+
+        # env
+        self.stage = omni.usd.get_context().get_stage()
+
+        # event
+        self.event = "move" # action event
+        self.total_event_count = 0 # event time
+        self.event_elapsed = 0 # event elapsed time
+        self.event_pool = [] # event pool
+        self.robot = robot
+        self.gripper = self.robot.gripper
+        self.cs_controller = RMPFlowController(name="cspace_controller", robot_articulation=self.robot)
+        
+        # TODO：find height
+        self.ee_pos_target = np.array([0.0, 0.0, 1.0])
+        self.ee_ori_target = np.array([1.0, 0.0, 0.0, 0.0])
+
+        # connection
+        self.connect_server = connect_server
+        # if connect_server:
+        #     self.client = KinovaClient()
+        #     self.sending_message = False
+       
+        # add go home default action
+        # self.apply_high_level_action()
+        # self.sending_message = False
+
+    def add_event_to_pool(self, event: str, elapsed: int, 
+                          ee_pos: np.ndarray, ee_ori: np.ndarray, gripper_ratio: float = 1.0):
+        self.event_pool.append([event, elapsed, ee_pos, ee_ori, gripper_ratio])
+        
+    def update_ee_target(self, pos, ori):
+        """
+        Update End-Effector Target position and orientation
+        """
+        self.ee_pos_target = pos
+        self.ee_ori_target = ori
+
+    def update_event(self, event: str):
+        """
+        Update robot high-level event
+        """
+        if event != self.event:
+            self.event = event
+            self.total_event_count = 0
+
+################################## sync robot ##################################
+    # def synchronize_robot(self):
+    #     """
+    #     Send message to the Server to 
+    #     """
+    #     if not self.sending_message:
+    #         # get joint positions and gripper degree
+    #         all_positions = self.robot.get_joint_positions()
+    #         gripper_degree = all_positions[7] / 0.8757
+    #         joint_positions = [regulate_degree(e, indegree=False) for e in all_positions[:7]]
+    #         joint_positions = joint_positions + [gripper_degree]
+
+    #         assert len(joint_positions) == 8, "Invalid number of joint positions"
+
+    #         # send message
+    #         message = " ".join([str(e) for e in joint_positions])
+        
+    #         self.sending_message = True
+    #         self.client.send_message("Control", message)
+    #         self.sending_message = False
+
+    # def obtain_robot_state(self):
+    #     """
+    #     Get robot state from the Server
+    #     """
+    #     if not self.sending_message:
+    #         self.sending_message = True
+    #         answer_message = self.client.send_message("GetJoints", "NA")
+    #         self.sending_message = False
+    #         return [float(e) for e in answer_message.split(" ")]
+
+    # def apply_high_level_action(self, action_name: str = "go_home"):
+    #     """
+    #     Apply high-level action to the robot
+    #     """
+    #     action = kinova_action_config[action_name]
+    #     if action['base_prim'] is None:
+    #         base_world_pos, base_world_rot = self.robot.get_world_pose()
+    #     else:
+    #         base_prim = XFormPrim(action['base_prim'])
+    #         base_world_pos, base_world_rot = base_prim.get_world_pose()
+        
+    #     base_mat = get_transform_mat_from_pos_rot(base_world_pos, base_world_rot)
+    #     print("base_mat", base_mat)
+        
+    #     for action_step in action['steps']:
+
+    #         step_type = action_step['action_type']
+    #         duration = action_step['duration']
+
+    #         if step_type == "move":
+    #             offset_mat = get_transform_mat_from_pos_rot(action_step['position'], action_step['orientation'])
+    #             print("offset_mat", offset_mat)
+
+    #             target_mat = offset_mat * base_mat 
+    #             print("target_mat", target_mat.ExtractTranslation(), target_mat.ExtractRotationQuat())
+
+    #             target_pos = target_mat.ExtractTranslation()
+    #             target_rot = target_mat.ExtractRotationQuat()
+
+    #             pos_array = np.array([target_pos[0], target_pos[1], target_pos[2]])
+    #             rot_array = np.array([target_rot.GetReal(), target_rot.GetImaginary()[0], target_rot.GetImaginary()[1], target_rot.GetImaginary()[2]])
+
+    #             self.add_event_to_pool(step_type, duration, pos_array, rot_array)
+    #         elif step_type in ["close", "open"]: 
+    #             gripper_ratio = action_step['ratio']
+    #             self.add_event_to_pool(step_type, duration, None, None, gripper_ratio)
+    #         elif step_type == "slerp":
+    #             slerp_action_sequence = generate_slerp_action_sequence(
+    #                 action_step['position'], 
+    #                 action_step['orientation'],
+    #                 action_step['relative_rotation'], 
+    #                 sub_steps=action_step['sub_steps'],
+    #                 sub_duration=action_step['duration'] // action_step['sub_steps'],
+    #                 slerp_last=action_step['slerp_last'],
+    #                 slerp_offset=action_step['slerp_offset']
+    #                 )
+
+    #             print("action_sequence", slerp_action_sequence)
+    #             for sub_action in slerp_action_sequence:
+    #                 offset_mat = get_transform_mat_from_pos_rot(sub_action['position'], sub_action['orientation'])
+    #                 target_mat = offset_mat * base_mat 
+    #                 target_pos = target_mat.ExtractTranslation()
+    #                 target_rot = target_mat.ExtractRotationQuat()
+
+    #                 pos_array = np.array([target_pos[0], target_pos[1], target_pos[2]])
+    #                 rot_array = np.array([target_rot.GetReal(), target_rot.GetImaginary()[0], target_rot.GetImaginary()[1], target_rot.GetImaginary()[2]])
+
+    #                 self.add_event_to_pool(sub_action['action_type'], sub_action['duration'], pos_array, rot_array)
+
+
+    def forward(self):
+        """
+        Main function to update the robot
+        """
+        # update event
+        if len(self.event_pool) > 0:
+            if self.event_elapsed <= 0:
+                event, elapsed, ee_pos, ee_ori, gripper_ratio = self.event_pool.pop(0)
+                print("event, elapsed, ee_pos, ee_ori ", event, elapsed, ee_pos, ee_ori, gripper_ratio)
+                self.update_event(event)
+                if self.event == "move":
+                    self.update_ee_target(ee_pos, ee_ori)
+                elif self.event == "close":
+                    self.gripper.set_close_ratio(gripper_ratio)
+                
+
+                if self.connect_server:
+                    self.synchronize_robot()
+
+                self.event_elapsed = elapsed
+        else:
+            if self.connect_server:
+                if self.total_event_count > 200 and self.total_event_count % (60 * 3) == 0:
+                    self.synchronize_robot()
+
+        # print("coffee control event", self.event, self.event_elapsed)
+        if self.event == "move":
+            actions = self.cs_controller.forward(
+                    target_end_effector_position=self.ee_pos_target,
+                    target_end_effector_orientation=self.ee_ori_target)    
+        elif self.event == "close":
+            actions = self.gripper.forward(action="close")
+        elif self.event == "open":
+            actions = self.gripper.forward(action="open")
+
+        self.robot.apply_action(actions)
+
+        # from omni.isaac.core.utils.types import ArticulationAction
+        # joint_actions = ArticulationAction()
+    
+        # joint_actions.joint_positions = [0, 15, 180, -130, 0, 55, 90] + [0.8] * 6
+        # for i in range(13):
+        #     joint_actions.joint_positions[i] = np.deg2rad(joint_actions.joint_positions[i])
+
+        # print("joint_actions", joint_actions)
+
+        
+        # self.robot.apply_action(joint_actions)
+        
+        self.total_event_count += 1 # update event time
+        self.event_elapsed -= 1 # update event elapsed time
+        # synchronize
+        # if self.connect_server:
+        #     if self.total_event_count % 60 == 0:
+        #         self.synchronize_robot()
+
+
+        # return actions
+    
+    
